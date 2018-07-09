6ce8bdad64837010ae11e248a5d1bce495853ff5
Damiani Liborio Igor <igor.damiani@hotmail.com>
2018-07-09 17:09:06 +0200

    -

---
+++ ProvVe.MachineService.ConsoleHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using ProvVe.MachineService.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvVe.MachineService.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost
+                (typeof(DeviceService),
+                new Uri("http://localhost:12345"));
+
+            host.Open();
+            Console.WriteLine("WCF started...");
+            Console.ReadLine();
+            host.Close();
+            host = null;
+        }
+    }
+}
---
+++ ProvVe.MachineService.WindowsServiceHost/Service1.cs
@@ -0,0 +1,63 @@
+﻿using ProvVe.MachineService.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvVe.MachineService.WindowsServiceHost
+{
+    public partial class Service1 : ServiceBase
+    {
+        ServiceHost host;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            host = new ServiceHost(typeof(IDeviceService));
+            host.Opening += Host_Opening;
+            host.Opened += Host_Opened;
+            host.Faulted += Host_Faulted;
+            host.Open();
+        }
+
+        private void Host_Faulted(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Host_Opened(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Host_Opening(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected override void OnStop()
+        {
+            host.Close();
+        }
+
+        protected override void OnContinue()
+        {
+            base.OnContinue();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+        }
+    }
+}
